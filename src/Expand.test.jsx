3fec2fa9085cf570ed46a2c87681045cc4e68055
Binh Le <sonybinhle@gmail.com>
2018-03-08 19:48:52 +0100

    Add more test to pass coverage of 70%

--- src/Expand.test.jsx
+++ src/Expand.test.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 
 import Expand from './Expand';
+
+jest.useFakeTimers();
 
 describe('Expand', () => {
   it('when render expand with children --> then render default Expand with children', () => {
     const wrapper = render(<Expand>Hello</Expand>);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('when render expand with open=true --> then render Expand with open styles', () => {
+    const wrapper = render(<Expand open>Open true</Expand>);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('when toggle open from false to true --> then change Expand styles from close to open', () => {
+    const wrapper = mount(<Expand>Toggle</Expand>);
+
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.setProps({ open: true });
+
+    jest.runOnlyPendingTimers();
+
+    wrapper.update();
+
+    expect(wrapper).toMatchSnapshot();
+
+    jest.runOnlyPendingTimers();
+
+    wrapper.update();
+
+    expect(wrapper).toMatchSnapshot();
+
+    expect(setTimeout).toHaveBeenCalledTimes(2);
   });
 });
